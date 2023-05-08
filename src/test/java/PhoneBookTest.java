c2b6cb975e91fb242bf6239975bda0a9168084bb
LoneDog001 <pomeshikov-andrey@mail.ru>
2023-05-08 20:27:30 +0400

    Stub and tests for findByNumber

--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -11,19 +11,19 @@ public class PhoneBookTest {
     private static PhoneBook phoneBookTest;
 
     @BeforeAll
-    public static void started(){
-            System.out.println("All test starts!");
-            phoneBookTest = new PhoneBook();
-        }
+    public static void started() {
+        System.out.println("All test starts!");
+        phoneBookTest = new PhoneBook();
+    }
 
-        @ParameterizedTest
-        @MethodSource("sourceAdd")
-        public void testAdd(String inputName, String inputNumber, int expected){
-        int result = phoneBookTest.add(inputName,inputNumber);
+    @ParameterizedTest
+    @MethodSource("sourceAdd")
+    public void testAdd(String inputName, String inputNumber, int expected) {
+        int result = phoneBookTest.add(inputName, inputNumber);
         assertEquals(expected, result);
     }
 
-    public static Stream <Arguments> sourceAdd(){
+    public static Stream<Arguments> sourceAdd() {
         return Stream.of(
                 Arguments.of("Andrey", "44444", 1),
                 Arguments.of("Kolya", "4521477", 2),
@@ -31,7 +31,24 @@ public class PhoneBookTest {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("sourceFindByName")
+    public void testFindByNumber(String inputName, int expected) {
+        phoneBookTest.add("Andrey", "44444");
+        String result = phoneBookTest.findByNumber(inputName);
+        assertEquals(expected, result);
+    }
 
-
-
+    public static Stream<Arguments> sourceFindByName() {
+        return Stream.of(
+                Arguments.of("", "44444"),
+                Arguments.of(null, "747546"),
+                Arguments.of(null, "1247856")
+        );
+    }
 }
+
+
+
+
+
