5d6285bef9203420be27c7ebc68b7ab95c455dfd
LoneDog001 <pomeshikov-andrey@mail.ru>
2023-05-08 20:17:18 +0400

    add implemented

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,14 @@
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class PhoneBook {
-    public int add (String name, String number){
-        return 0;
+    TreeMap<String, String> phoneBook = new TreeMap<>();
+    HashMap<String, String> reversedKeyBook = new HashMap<>();
+
+    public int add(String name, String number) {
+        if (phoneBook.put(name, number) == null) {
+            reversedKeyBook.put(number, name);
+        }
+        return phoneBook.size();
     }
 }
